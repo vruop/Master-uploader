968fb8dd5615c9230e9151702277948f98a97718
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-16 23:51:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    AUTH_USER = os.environ.get('AUTH_USERS', '7602994049').split(',')
+    AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+    HOST = "https://drm-api-six.vercel.app"
+    CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
