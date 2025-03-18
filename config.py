0b78b64958ec7e5f519033d49f3aeaa579f62561
Ayush <112577569+Ayus444@users.noreply.github.com>
2025-03-18 18:22:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7602994049').split(',')
+    BOT_TOKEN = os.environ.get("7756457260:AAFQPEAIH4Dh-OLf3sGCGAfUzGfuWiVMyAc")
+    API_ID = int(os.environ.get("22581733"))
+    API_HASH = os.environ.get("1db7bdcf908100cc641c6a5276765c3d")
+    AUTH_USER = os.environ.get('AUTH_USERS', '6530997270').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-six.vercel.app"
+    HOST = ""
     CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
