c83f4d06640eea01f6a9d88662d3029f844802cb
vruop <q803816@gmail.com>
2025-07-09 14:19:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7285235045:AAHr2u01d4zEY_vimFa1PUfXJReJJK_GJ20")  # Ensure correct key name
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8135851585:AAH8PI5DmoLQJ_7rqLwudB0PTI__7DcMygs")  # Ensure correct key name
     API_ID = int(os.environ.get("API_ID", "25566754"))  # Added key name and default value
     API_HASH = os.environ.get("API_HASH", "27609daf8ee847978797a359024e7607")  # Added key name for consistency
 
@@ -9,4 +9,4 @@ class Config(object):
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
-    CREDIT = os.environ.get("CREDIT", "SACHIN 𝘽𝙊𝙏𝙎")  # Making CREDIT an environment variable for flexibility
+    CREDIT = os.environ.get("CREDIT", "--ANJAN PERSON--")  # Making CREDIT an environment variable for flexibility
